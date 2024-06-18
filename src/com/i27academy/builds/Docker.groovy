2219c108275ccbdcc48d781c1349bfe28c140f76
nzalpha <148647509+nzalpha@users.noreply.github.com>
2024-06-18 00:11:29 -0700

    first commit

---
+++ src/com/i27academy/builds/Docker.groovy
@@ -0,0 +1,21 @@
+package com.i27academy.builds
+
+class Docker{
+    def jenkins
+    Docker(jenkins){
+        this.jenkins=jenkins
+    }
+
+    def add(fnum,snum){
+        return fnum+snum
+    }
+
+    //Application Build
+
+    //Docker Build
+
+
+    // Docker Login
+
+    // Docker push
+}
---
+++ vars/dockerPipeline.groovy
@@ -0,0 +1,32 @@
+import com.i27academy.builds.Docker
+
+def call(Map pipelineParams){
+    Docker d = new Docker(this)
+}
+
+pipeline {
+   agent {
+        label 'K8-slave'
+    }
+    tools {
+        // maven 'mvn 3.8'
+        jdk 'jdk-17'
+    }
+
+environment {
+        APPLICATION_NAME = {pipelineParams.appName}
+
+    }
+
+    stages{
+        stage ('Add') {
+            // This step will take care of building the application
+            steps {
+                echo "--------Add Method-------"
+                script{
+                    println d.add(5,6)
+                }
+            }
+        }
+    }
+}
