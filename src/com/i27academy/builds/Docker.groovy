c32b4fbee06620f3c12da1fe251ca14ba79803f8
nzalpha <148647509+nzalpha@users.noreply.github.com>
2024-07-01 15:52:10 -0700

    newch

--- src/com/i27academy/builds/Docker.groovy
+++ src/com/i27academy/builds/Docker.groovy
@@ -63,6 +63,10 @@ class Docker{
         echo "dockerHub is $dockerHub"
         echo "VMCredUsr is $VMCredUsr"
         echo "VMCredPsw usr is $VMCredPsw"
+        script{
+                 sshpass -p $VMCredPsw ssh -o StrictHostKeyChecking=no $VMCredUsr@$dockerServerIp docker pull $dockerHub/$appName:$gitCommit  
+               }
+
         """
 }
 
